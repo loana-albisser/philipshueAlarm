3c3b6ed5b461959127c69923360538d33dc0ede0
Loana Albisser <loana.albisser@peax.ch>
2021-09-11 11:43:38 +0200

    add weekday list

--- app/src/main/java/com/loanaalbisser/philipshueAlarm/addAlarm/AddAlarmScreen.kt
+++ app/src/main/java/com/loanaalbisser/philipshueAlarm/addAlarm/AddAlarmScreen.kt
@@ -1,27 +1,26 @@
 package com.loanaalbisser.philipshueAlarm.addAlarm
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.loanaalbisser.philipshueAlarm.LightsList
-import com.loanaalbisser.philipshueAlarm.hue.Light
 
 @ExperimentalMaterialApi
 @Composable
 fun AddAlarmScreen(viewModel: AddAlarmViewModel = viewModel()) {
     Column(modifier = Modifier.padding(8.dp)) {
         LightList(viewModel = viewModel)
-        BrightnessComponent()
+        BrightnessComponent("Start Brightness")
+        BrightnessComponent("End Brightness")
+        WeekdayListComponent()
     }
-
 }
 
 @Composable
@@ -34,18 +33,17 @@ fun LightList(viewModel: AddAlarmViewModel) {
         elevation = 10.dp
     ) {
         Column(
-            modifier = Modifier.padding(15.dp)
+            modifier = Modifier.padding(16.dp)
         ) {
             val lights = viewModel.getLights()
             LightsList(lights, onCheckedChange = viewModel::onLightStateChanged)
         }
     }
-
 }
 
 @ExperimentalMaterialApi
 @Composable
-fun BrightnessComponent() {
+fun BrightnessComponent(title: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -56,17 +54,17 @@ fun BrightnessComponent() {
         Column(
             modifier = Modifier.padding(15.dp)
         ) {
-            BrightnessTitle()
+            Title(title)
             BrightnessSlider()
         }
     }
 }
 
 @Composable
-fun BrightnessTitle() {
+fun Title(title: String) {
     Text(
         color = Color.White,
-        text = "Brightness")
+        text = title)
 }
 
 
@@ -89,6 +87,63 @@ fun BrightnessSlider() {
             activeTrackColor = MaterialTheme.colors.secondary
         )
     )
+}
+
+@Composable
+fun WeekdayListComponent() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable { },
+        elevation = 10.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(15.dp),
+        ) {
+            Title("Repetition")
+            WeekdayList()
+        }
+    }
+}
+
+@Composable
+fun WeekdayList() {
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center) {
+        SingleWeekday(text = "M")
+        SingleWeekday(text = "D")
+        SingleWeekday(text = "M")
+        SingleWeekday(text = "D")
+        SingleWeekday(text = "F")
+        SingleWeekday(text = "S")
+        SingleWeekday(text = "S")
+    }
+
+}
+
+@Composable
+fun SingleWeekday(text: String) {
+    Surface(
+        modifier = Modifier
+            .size(40.dp)
+            .padding(4.dp),
+        // border = BorderStroke(2.dp, Color.Red),
+        contentColor = Color.Blue,
+        elevation = 8.dp,
+        color = Color.White,
+        shape = CircleShape,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = text
+            )
+        }
+    }
 }
 
 
